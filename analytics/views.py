f61cce5649d52a857eb2b4c53f1d36285f028297
Nuurs <tolegennursultan303@gmail.com>
2024-11-24 01:52:35 +0500

    suiii

--- analytics/views.py
+++ analytics/views.py
@@ -6,6 +6,5 @@ def analytics_dashboard(request):
     # Get active users
     active_users = ApiRequest.objects.values('user').annotate(request_count=Count('id')).order_by('-request_count')
     
-    # Optionally, add other analytics data here
 
     return render(request, 'analytics/dashboard.html', {'active_users': active_users})
